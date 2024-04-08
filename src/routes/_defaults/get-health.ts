885a48dbabcd34d220e4fb2e0f2e860e97fbb1af
JOAO_VICTOR <joaovictormendesavila@gmail.com>
2024-04-07 23:55:58 -0300

    fix(routes): fix health route

--- src/routes/_defaults/get-health.ts
+++ src/routes/_defaults/get-health.ts
@@ -2,7 +2,7 @@ import { FastifyInstance } from "fastify";
 import { ZodTypeProvider } from "fastify-type-provider-zod";
 
 export async function getHealth(app: FastifyInstance) {
-  app.withTypeProvider<ZodTypeProvider>().get('/healthz', async (request, reply) => {
+  app.withTypeProvider<ZodTypeProvider>().get('/health', async (request, reply) => {
     return reply.status(200).send("HTTP server healthy!")
   })
 }
